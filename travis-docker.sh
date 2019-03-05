e0295d5a13c5c10b08b4a22ae2fd648bd84511e8
Allan Targino <13934447+allantargino@users.noreply.github.com>
2019-03-05 01:19:56 -0300

    changing jar file in dockerfile

--- travis-docker.sh
+++ travis-docker.sh
@@ -4,6 +4,6 @@ set -e
 
 docker build -t kube-repo-validator .
 docker images
-docker tag kube-repo-validator $DOCKER_USERNAME/kube-repo-validator
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_USERNAME/kube-repo-validator
+docker tag kube-repo-validator $DOCKER_REGISTRY/kube-repo-validator
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin $DOCKER_REGISTRY
+docker push $DOCKER_REGISTRY/kube-repo-validator
